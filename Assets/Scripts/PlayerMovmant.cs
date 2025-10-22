e855427a459bac5c3bbd7b0fc21afe5ac2a5b2f1
dima <dhavryliuk@edu.cdv.pl>
2025-10-22 09:03:02 +0200

    playerMovment

--- Assets/Scripts/PlayerMovmant.cs
+++ Assets/Scripts/PlayerMovmant.cs
@@ -3,24 +3,32 @@ using UnityEngine;
 
 public class PlayerMovmant : MonoBehaviourPunCallbacks
 {
-    [SerializeField] CharacterController characterController;
-    [SerializeField] float speed = 2.0f;
-    [SerializeField] float gravity = 9.8f;
-    [SerializeField] float jumpForce = 10;
+    //[SerializeField] CharacterController characterController;
+    //[SerializeField] float speed = 2.0f;
+    //[SerializeField] float gravity = 9.8f;
+    //[SerializeField] float jumpForce = 10;
+
+    [SerializeField] private float sensivity = 1;
     
     float yForce = 0;
     void Start()
     {
-        if (photonView.IsMine) Camera.main.transform.SetParent(transform);
+        if (photonView.IsMine)
+        {
+            Camera.main.transform.SetParent(transform);
+            Camera.main.transform.localPosition = Vector3.zero;
+            Camera.main.transform.localRotation = Quaternion.identity;
+            transform.LookAt(Vector3.zero);
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
         else enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if(Input.GetButtonDown("Jump")) yForce = jumpForce;
-        characterController.Move((direction.normalized*speed+new Vector3(0, yForce, 0))*Time.deltaTime);
-        yForce -= gravity *Time.deltaTime;
+        Camera.main.transform.localRotation = Quaternion.Euler(Mathf.Clamp((Camera.main.transform.localRotation.eulerAngles.x - Input.mousePositionDelta.y+180)%360-180, -60, 60), Mathf.Clamp((Camera.main.transform.localRotation.eulerAngles.y + Input.mousePositionDelta.x+180)%360-180, -90, 90), 0);
     }
 }
--- Assets/Scripts/SampleLauncher.cs
+++ Assets/Scripts/SampleLauncher.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SampleLauncher : MonoBehaviourPunCallbacks
 {
     public PhotonView playerPrefab;
+	[SerializeField] private Transform[] spawnPoints;
     /*void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -22,14 +23,38 @@ public class SampleLauncher : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity, 0);
     }*/
 
+    [PunRPC]
+    private void getSpawnPoint(PhotonMessageInfo info)
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (spawnPoints[i].gameObject.activeSelf)
+            {
+                photonView.RPC("sendSpawnPoint", info.Sender, i);
+                spawnPoints[i].gameObject.SetActive(false);
+                return;
+            }
+        }
+    }
+
+    [PunRPC]
+    private void sendSpawnPoint(int index)
+    {
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoints[index].position, Quaternion.identity, 0);
+    }
+
     public void Start()
     {
-        if(PhotonNetwork.IsMasterClient) PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity, 0);
+        if (PhotonNetwork.IsMasterClient)
+        {
+            sendSpawnPoint(0);
+            spawnPoints[0].gameObject.SetActive(false);
+        }
     }
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity, 0);
+        photonView.RPC("getSpawnPoint", RpcTarget.MasterClient);
         base.OnJoinedRoom();
     }
 
