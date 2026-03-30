2b877d77e9d49ff53425906e5a50186453f8df31
dima <dhavryliuk@edu.cdv.pl>
2026-03-30 17:04:26 +0200

    1

--- Assets/Scripts/BlinkingLight.cs
+++ Assets/Scripts/BlinkingLight.cs
@@ -6,6 +6,7 @@ public class BlinkingLight : MonoBehaviour
     [SerializeField] Vector2 ON_time = new Vector2(5, 10);
     [SerializeField] Vector2 OFF_time = new Vector2(0.01f, 0.1f);
     [SerializeField] Light light;
+    [SerializeField] GameObject gameObject;
     void Start()
     {
         StartCoroutine(Blink());
@@ -16,8 +17,10 @@ public class BlinkingLight : MonoBehaviour
         while (true)
         {
             light.enabled = true;
+            if(gameObject) gameObject.SetActive(true);
             yield return new WaitForSeconds(Random.Range(ON_time.x, ON_time.y));
             light.enabled = false;
+            if(gameObject) gameObject.SetActive(false);
             yield return new WaitForSeconds(Random.Range(OFF_time.x, OFF_time.y));
         }
     }
